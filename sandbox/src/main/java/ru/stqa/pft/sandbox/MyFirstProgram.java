c0eab6c51dbefef17c2b33c86c06798a2d7ceb58
ShevkoplyasDarya <shevkoplyund@gmail.com>
2019-10-03 09:55:55 +0300

    Задания урока 1.9

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,9 +1,16 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-	
-public static void main(String[] args) {	
-System.out.println("Hello, world");
-}
+
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+        System.out.println(2 + 2);
+        System.out.println(2 * 2);
+        System.out.println(2 / 2);
+        System.out.println(2 - 2);
+        System.out.println(1 / 2);
+        System.out.println(1.0 / 2);
+
+    }
 
 }
