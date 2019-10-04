4f48a81ae3110fb74c12cfb724e1791e506b76e1
ShevkoplyasDarya <shevkoplyund@gmail.com>
2019-10-04 09:48:34 +0300

    Вернулись к началу

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,12 +4,7 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
         System.out.println("Hello, world");
-        System.out.println(2 + 2);
-        System.out.println(2 * 2);
-        System.out.println(2 / 2);
-        System.out.println(2 - 2);
-        System.out.println(1 / 2);
-        System.out.println(1.0 / 2);
+       
 
     }
 
